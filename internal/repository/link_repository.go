bf2c3e944f5b197ebd8d3632fb2e997f25bb9706
jong <jochong27@gmail.com>
2025-06-23 16:02:46 +0200

    update repository

--- internal/repository/link_repository.go
+++ internal/repository/link_repository.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	"fmt"
-
 	"github.com/axellelanca/urlshortener/internal/models"
 	"gorm.io/gorm"
 )
@@ -16,44 +14,50 @@ type LinkRepository interface {
 	CountClicksByLinkID(linkID uint) (int, error)
 }
 
-// TODO :  GormLinkRepository est l'implémentation de LinkRepository utilisant GORM.
+// GormLinkRepository est l'implémentation de LinkRepository utilisant GORM.
 type GormLinkRepository struct {
+	db *gorm.DB
 }
 
 // NewLinkRepository crée et retourne une nouvelle instance de GormLinkRepository.
 // Cette fonction retourne *GormLinkRepository, qui implémente l'interface LinkRepository.
 func NewLinkRepository(db *gorm.DB) *GormLinkRepository {
-	// TODO
+	return &GormLinkRepository{
+		db: db,
+	}
 }
 
 // CreateLink insère un nouveau lien dans la base de données.
 func (r *GormLinkRepository) CreateLink(link *models.Link) error {
-	// TODO 1: Utiliser GORM pour créer un nouvel enregistrement (link) dans la table des liens.
-
+	result := r.db.Create(link)
+	return result.Error
 }
 
 // GetLinkByShortCode récupère un lien de la base de données en utilisant son shortCode.
 // Il renvoie gorm.ErrRecordNotFound si aucun lien n'est trouvé avec ce shortCode.
 func (r *GormLinkRepository) GetLinkByShortCode(shortCode string) (*models.Link, error) {
 	var link models.Link
-	// TODO 2: Utiliser GORM pour trouver un lien par son ShortCode.
-	// La méthode First de GORM recherche le premier enregistrement correspondant et le mappe à 'link'.
-
+	result := r.db.Where("short_code = ?", shortCode).First(&link)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &link, nil
 }
 
 // GetAllLinks récupère tous les liens de la base de données.
 // Cette méthode est utilisée par le moniteur d'URLs.
 func (r *GormLinkRepository) GetAllLinks() ([]models.Link, error) {
 	var links []models.Link
-	// TODO 3: Utiliser GORM pour récupérer tous les liens.
-
+	result := r.db.Find(&links)
+	return links, result.Error
 }
 
 // CountClicksByLinkID compte le nombre total de clics pour un ID de lien donné.
 func (r *GormLinkRepository) CountClicksByLinkID(linkID uint) (int, error) {
 	var count int64 // GORM retourne un int64 pour les comptes
-	// TODO 4: Utiliser GORM pour compter les enregistrements dans la table 'clicks'
-	// où 'LinkID' correspond à l'ID du lien donné.
-
+	result := r.db.Model(&models.Click{}).Where("link_id = ?", linkID).Count(&count)
+	if result.Error != nil {
+		return 0, result.Error
+	}
 	return int(count), nil
 }
